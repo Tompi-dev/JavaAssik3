1472630222ece3d3d2f8801558e2e1beb18bf82e
Tompi-dev <tomiris.kassymova.98@gmail.com>
2025-10-25 13:01:03 +0500

    fix(imports): resolved PerformanceTracker reference in UnionFind

--- src/main/java/metrics/PerformanceTracker.java
+++ src/main/java/metrics/PerformanceTracker.java
@@ -1,4 +1,27 @@
 package metrics;
 
 public class PerformanceTracker {
+    private long comparisons = 0;
+    private long swaps = 0;
+    private long arrayAccesses = 0;
+
+    public void incComparisons() { comparisons++; }
+    public void incSwaps() { swaps++; }
+    public void incArrayAccesses(int count) { arrayAccesses += count; }
+
+    public long getComparisons() { return comparisons; }
+    public long getSwaps() { return swaps; }
+    public long getArrayAccesses() { return arrayAccesses; }
+
+    public void reset() {
+        comparisons = 0;
+        swaps = 0;
+        arrayAccesses = 0;
+    }
+
+    public String report() {
+        return "Comparisons=" + comparisons +
+                ", Swaps=" + swaps +
+                ", ArrayAccesses=" + arrayAccesses;
+    }
 }
--- src/test/java/algorithms/TestGraph.java
+++ src/test/java/algorithms/TestGraph.java
@@ -1,4 +1,15 @@
-package algorithms;
+package cli;
+
+import graph.Graph;
 
 public class TestGraph {
+    public static void main(String[] args) {
+        Graph g = new Graph(5);
+        g.addEdge(0, 1, 10);
+        g.addEdge(0, 2, 5);
+        g.addEdge(1, 3, 7);
+        g.addEdge(3, 4, 2);
+
+        System.out.println(g);
+    }
 }
